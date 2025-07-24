b6010de984e907a05717d35aec7def923fd70785
Sri Gnana Guru <155650073+srignanaguru@users.noreply.github.com>
2025-07-24 21:53:27 +0530

    Update valid_parenthesis.py

--- stack/valid_parenthesis.py
+++ stack/valid_parenthesis.py
@@ -3,12 +3,14 @@ def checkparen(s):
     dic={')':'(',']':'[','}':'{'}
 
     for c in s:
-        if c in dic.values():
+        if c in dic.values(): 
             stack.append(c)
         elif c in dic:
-            if not stack or stack[-1]!=dic[c]:
+            if not stack or stack[-1]!=dic[c]: # Checking whether the stack is empty or current char is not equal to the dic of key & value pair
                 return False
             stack.pop()
+        else:
+            return False
     
     return True
 
